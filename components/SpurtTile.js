04f78530c992f2830cfc648e85e49581d490d87e
Danny <danny.f.gomez@gmail.com>
2025-06-15 23:25:54 -0400

    style spurt component

--- components/SpurtTile.js
+++ components/SpurtTile.js
@@ -1,9 +1,43 @@
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
 function SpurtTile() {
-  return <Text>Spurt Tile</Text>;
+  return (
+    <View style={styles.mainContainer}>
+      <View>
+        <Text>Exercise Icon</Text>
+      </View>
+      <View style={styles.exerciseInfo}>
+        <Text style={styles.title}>Exercise Title</Text>
+        <Text>Time to Complete</Text>
+      </View>
+    </View>
+  );
 }
 
 export default SpurtTile;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mainContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FE7F2D',
+    width: 'auto',
+    padding: 16,
+    gap: 16,
+    borderTopLeftRadius: 24,
+    borderTopRightRadius: 0,
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 24,
+    margin: 24,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    alignItems: 'center',
+    color: '#233D4D',
+  },
+  exerciseInfo: {
+    
+  },
+});
