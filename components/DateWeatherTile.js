e8c39947a28fe4c31895c0cbb4454b3b61a78d86
Danny <danny.f.gomez@gmail.com>
2025-06-16 23:41:44 -0400

    add styling to date weather tile

--- components/DateWeatherTile.js
+++ components/DateWeatherTile.js
@@ -1,13 +1,29 @@
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 function DateWeatherTile() {
   return (
-    <View>
+    <View style={styles.mainContainer}>
       <Text>Date</Text>
-      <Text>Weather</Text>
       <Text>Weather Icon</Text>
+      <Text>Weather Temp</Text>
     </View>
   );
 }
 
 export default DateWeatherTile;
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#A1C181',
+    padding: 16,
+    gap: 12,
+    borderTopLeftRadius: 24,
+    borderTopRightRadius: 0,
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 24,
+    margin: 24,
+  },
+});
