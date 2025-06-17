e2ce46abc7c946914ae244ab30ce253012f5a40e
Danny <danny.f.gomez@gmail.com>
2025-06-16 23:32:35 -0400

    create dateweathertile

---
+++ components/DateWeatherTile.js
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+
+function DateWeatherTile() {
+  return (
+    <View>
+      <Text>Date</Text>
+      <Text>Weather</Text>
+      <Text>Weather Icon</Text>
+    </View>
+  );
+}
+
+export default DateWeatherTile;
