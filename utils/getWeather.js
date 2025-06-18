8f145b9591675c43c211aff3a74870971e4d1568
Danny <danny.f.gomez@gmail.com>
2025-06-18 00:12:12 -0400

    return weather temp in imperial

--- utils/getWeather.js
+++ utils/getWeather.js
@@ -13,7 +13,7 @@ export async function getWeather() {
 
     const API_KEY = Constants.expoConfig?.extra?.openWeatherApiKey;
     console.log('key', API_KEY);
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${API_KEY}`;
 
     const response = await fetch(url);
     const data = await response.json();
