0f3cbe25c1e27314b1164f5a0b1030477fceb1ab
Danny <danny.f.gomez@gmail.com>
2025-06-18 00:04:03 -0400

    install expo constants, utilize process env to keep key secret

--- utils/getWeather.js
+++ utils/getWeather.js
@@ -1,4 +1,5 @@
 import * as Location from 'expo-location';
+import Constants from 'expo-constants';
 
 export async function getWeather() {
   try {
@@ -10,8 +11,8 @@ export async function getWeather() {
     const location = await Location.getCurrentPositionAsync({});
     const { latitude, longitude } = location.coords;
 
-    const API_KEY = process.env.OPENWEATHER_API_KEY;
-    console.log("key", API_KEY)
+    const API_KEY = Constants.expoConfig?.extra?.openWeatherApiKey;
+    console.log('key', API_KEY);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`;
 
     const response = await fetch(url);
